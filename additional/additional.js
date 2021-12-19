cfd39b6a445cbef1c6ad3105775a7a47ed5435c3
Cherevanmisha <cherevanmisha@ukr.net>
2021-12-19 16:54:34 +0200

    Lesson12

--- additional/additional.js
+++ additional/additional.js
@@ -16,8 +16,10 @@ fetch('https://jsonplaceholder.typicode.com/users')
         for (const postElement of users) {
             let divElement = document.createElement('div');
             divElement.innerText = `${postElement.id} ${postElement.name} ${postElement.username} `
-            let buttonElement = document.createElement('button' );
 
+
+
+            let buttonElement = document.createElement('button' );
             buttonElement.innerText = 'open' ;
             buttonElement.onclick = function () {
                 fetch(`https://jsonplaceholder.typicode.com/users/${postElement.id}/posts`)
@@ -28,11 +30,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
                         for (const comentElement of posts) {
                             let com = document.createElement('div');
                             com.innerText = comentElement.body;
-                            coment.appendChild(com)
+                            coment.appendChild(com);
 
 
-                        }
+                            let htmlButtonElement = document.createElement('button');
+                            htmlButtonElement.innerText = 'add';
+                            htmlButtonElement.onclick = function () {
+                                fetch(`https://jsonplaceholder.typicode.com/posts/${postElement.id}/comments`)
+                                    .then(response => response.json())
+                                    .then(posts => {
 
+                    let htmlDivElement = document.createElement('div');
+                                        divElement.appendChild(htmlDivElement)
+                                        for (const comElement of posts) {
+                                            let htmlDivElement1 = document.createElement('div');
+                                        }
                     });
             };
             divElement.appendChild(buttonElement)
